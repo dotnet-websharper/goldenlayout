f85322e7b219ab8fb39a62f346fd62f047b0b21b
Ádám Abonyi-Tóth <atadi96@gmail.com>
2017-07-07 16:38:29 +0200

    Done binding the config objects

--- WebSharper.GoldenLayout/Main.fs
+++ WebSharper.GoldenLayout/Main.fs
@@ -4,7 +4,123 @@ open WebSharper
 open WebSharper.InterfaceGenerator
 
 module Definition =
-    let LayoutConfigClass = "LayoutConfig"
+
+    //Item config
+
+    let ComponentConfig =
+        Pattern.Config "Component" {
+            Required = [ "componentName", T<string> ]
+            Optional = [ "componentState", T<obj> ]
+        }
+
+    let ReactComponentConfig =
+        Pattern.Config "ReactComponent" {
+            Required = [ "component", T<string> ]
+            Optional = [ "props", T<obj> ]
+        }
+
+    let StackConfig = 
+        Pattern.Config "Stack" {
+            Required = []
+            Optional = [ "activeItemIndex", T<int> ]
+        }
+
+    let RowConfig =
+        Pattern.Config "Row" {
+            Required = []
+            Optional = []
+        }
+
+    let ColumnConfig =
+        Pattern.Config "Column" {
+            Required = []
+            Optional = []
+        }
+
+    let ItemConfig =
+        Pattern.Config "ItemConfig" {
+            Required =
+                [
+                    "type",
+                        ComponentConfig.Type +
+                        ReactComponentConfig.Type +
+                        StackConfig.Type +
+                        RowConfig.Type +
+                        ColumnConfig.Type
+                ]
+            Optional = 
+                [
+                    "content", Type.ArrayOf TSelf
+                    "width", T<int>
+                    "height", T<int>
+                    "id", T<string> + Type.ArrayOf T<string>
+                    "isClosavle", T<bool>
+                    "title", T<string>
+                ]
+        }
+
+    //Layout config 
+
+    let LayoutSettings =
+        Pattern.Config "LayoutSettings" {
+            Optional =
+                [
+                    "hasHeaders", T<bool>
+                    "constrainDragToContainer", T<bool>
+                    "reorderEnabled", T<bool>
+                    "selectionEnabled", T<bool>
+                    "popoutWholeStack", T<bool>
+                    "blockedPopoutsThrowError", T<bool>
+                    "closePopoutsOnUnload", T<bool>
+                    "showPopoutIcon", T<bool>
+                    "showMaximiseIcon", T<bool>
+                    "showCloseIcon", T<bool>
+                ]
+            Required = []
+        }
+
+    let LayoutDimensions =
+        Pattern.Config "LayoutDimensions" {
+            Optional =
+                [
+                    "borderWidth", T<int>
+                    "minItemHeight", T<int>
+                    "minItemWidth", T<int>
+                    "headerHeight", T<int>
+                    "dragProxyWidth", T<int>
+                    "dragProxyHeight", T<int>
+                ]
+            Required = []
+        }
+
+    let LayoutLabels =
+        Pattern.Config "LayoutLabels" {
+            Optional =
+                [
+                    "close", T<string>
+                    "maximise", T<string>
+                    "minimise", T<string>
+                    "popout", T<string>
+                ]
+            Required = []
+        }
+
+    let LayoutConfig =
+        Pattern.Config "Layout" {
+            Required =
+                [
+                    
+                ]
+            Optional = 
+                [
+                    "settings", LayoutSettings.Type
+                    "dimensions", LayoutDimensions.Type
+                    "labels", LayoutLabels.Type
+                    "content", Type.ArrayOf ItemConfig.Type
+                ]
+        }
+
+
 
     let Assembly =
         Assembly [
