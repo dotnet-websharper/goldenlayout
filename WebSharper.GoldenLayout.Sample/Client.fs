c932fb12a5090d038226c9898ec0d7465ba2a20a
Ádám Abonyi-Tóth <atadi96@gmail.com>
2017-07-11 18:09:51 +0200

    Sample project now kind of works

--- WebSharper.GoldenLayout.Sample/Client.fs
+++ WebSharper.GoldenLayout.Sample/Client.fs
@@ -5,10 +5,12 @@ open WebSharper.JavaScript
 open WebSharper.JQuery
 open WebSharper.UI.Next
 open WebSharper.UI.Next.Client
+open WebSharper.UI.Next.Html
 open WebSharper.UI.Next.Templating
 open WebSharper.GoldenLayout
 
-[<Require(typeof<GoldenLayout.Resources.DarkTheme>)>]
+[<Require(typeof<GoldenLayout.Resources.BaseCss>)>]
+[<Require(typeof<GoldenLayout.Resources.LightTheme>)>]
 [<JavaScript>]
 module Client =
 
@@ -18,25 +20,41 @@ module Client =
 
     [<SPAEntryPoint>]
     let Main () =
+
+        let component_ id = 
+            ItemFactory.CreateComponent(
+                Component(
+                    componentName = "example",
+                    ComponentState = ({Text = "This is the content of Component " + string id} :> obj)
+                ),
+                Item(Title = "My " + string id + ". component")
+            )
+        Console.Log component_
         let gl =
             GoldenLayout(
                 Layout(
                     Content = [|
                         ItemFactory.CreateRow(
                             Item(
-                                Content =
-                                    [|
-                                        (ItemFactory.CreateComponent(
-                                            Component(
-                                                componentName = "[1] Component",
-                                                ComponentState = ({Text = "This is the content of [1] Component"} :> obj)
-                                            ),
-                                            Item()))
-                                    |]
+                                Content = (
+                                    seq { 1..3 }
+                                    |> Seq.map (fun id -> component_ id)
+                                    |> Array.ofSeq
+                                )
                             )
                         )
                     |]
-                )
+                ),
+                JQuery.Of "#gl-cont"
             )
+        gl.RegisterComponent(
+            "example", 
+            fun (container, s) ->
+                let state = s :?> State
+                Console.Log state
+                container.GetElement().Text state.Text
+                |> ignore
+        )
+        gl.Init()
         Doc.Empty
-        |> Doc.RunById "main"
+        |> Doc.RunAppendById "main"
