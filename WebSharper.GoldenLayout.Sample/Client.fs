16a8f98913629f406118822737d7d340d7d49278
Ádám Abonyi-Tóth <atadi96@gmail.com>
2017-07-12 18:08:33 +0200

    More work on the sample project

--- WebSharper.GoldenLayout.Sample/Client.fs
+++ WebSharper.GoldenLayout.Sample/Client.fs
@@ -14,12 +14,13 @@ open WebSharper.GoldenLayout
 [<JavaScript>]
 module Client =
 
-    let a = 0
-
     type State = {Text: string}
+    type IndexTemplate = Template<"index.html", ClientLoad.FromDocument>
 
     [<SPAEntryPoint>]
     let Main () =
+
+        let tabName = Var.Create "filename"
 
         let component_ id = 
             ItemFactory.CreateComponent(
@@ -27,34 +28,64 @@ module Client =
                     componentName = "example",
                     ComponentState = ({Text = "This is the content of Component " + string id} :> obj)
                 ),
-                Item(Title = "My " + string id + ". component")
+                Item(
+                    Title = "My " + string id + ". component",
+                    IsClosable = true
+                )
             )
         Console.Log component_
         let gl =
             GoldenLayout(
                 Layout(
                     Content = [|
-                        ItemFactory.CreateRow(
+                        ItemFactory.CreateStack(
+                            Stack(),
                             Item(
                                 Content = (
                                     seq { 1..3 }
                                     |> Seq.map (fun id -> component_ id)
                                     |> Array.ofSeq
-                                )
+                                ),
+                                Id = Union1Of2 "main-window",
+                                IsClosable = false
                             )
                         )
-                    |]
+                    |],
+                    Settings = LayoutSettings(
+                        ShowCloseIcon = false,
+                        ShowMaximiseIcon = false,
+                        ShowPopoutIcon = false
+                    )
                 ),
-                JQuery.Of "#gl-cont"
+                JQuery.Of "#gl-container"
             )
         gl.RegisterComponent(
             "example", 
             fun (container, s) ->
                 let state = s :?> State
-                Console.Log state
-                container.GetElement().Text state.Text
+                (h2 [text state.Text]).Html
+                |> container.GetElement().Html
                 |> ignore
         )
         gl.Init()
-        Doc.Empty
-        |> Doc.RunAppendById "main"
+        //gl.Root.GetItemsById("main-window").[0]
+        IndexTemplate.AddTemplate()
+            .TabName(tabName)
+            .TabCreate(fun () ->
+                let component_ =
+                    ItemFactory.CreateComponent(
+                        Component(
+                            componentName = "example",
+                            ComponentState = {Text = tabName.Value + ": I was just created dynamically!"}
+                        ),
+                        Item(
+                            Title = tabName.Value
+                        )
+                    )
+                gl.Root.GetItemsById("main-window").[0].AddChild(component_)
+                Console.Log "length:"
+                Console.Log (gl.Root.GetItemsById("main-window").Length)
+            ).LogButton(fun () -> Console.Log gl)
+            .Doc()
+            
+        |> Doc.RunById "menu"
