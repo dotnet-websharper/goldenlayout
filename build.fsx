a05f997523adf7da470571631cf71a9f19977be8
Jand42 <andras.janko@gmail.com>
2017-09-07 18:48:26 +0200

    Update package title

--- build.fsx
+++ build.fsx
@@ -31,7 +31,7 @@ bt.Solution [
     bt.NuGet.CreatePackage()
         .Configure(fun c ->
             { c with
-                Title = Some "WebSharper.GoldenLayout"
+                Title = Some "WebSharper bindings for GoldenLayout"
                 LicenseUrl = Some "http://websharper.com/licensing"
                 ProjectUrl = Some "https://github.com/intellifactory/websharper.goldenlayout"
                 Description = "WebSharper extension for GoldenLayout"
